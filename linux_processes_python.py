8c0e0d59e3a66d6423c8dcfbf4d0db035611af9e
kurogane13 <akaisuzuki@gmail.com>
2023-05-11 21:57:58 -0300

    Fixed choices options

--- linux_processes_python.py
+++ linux_processes_python.py
@@ -1,5 +1,7 @@
 import psutil
 import os
+import subprocess
+import signal
 import easygui as eg
 
 def show_processes():
@@ -38,6 +40,8 @@ def kill_process():
     process_amount = os.popen("ps ax | grep -v 'ps aux\|grep' | wc -l").read()
     options = []
     for process in processes:
+        if len(process) < 2:
+            options.append("-")
         if process != '':
             process_data = process.split()
             pid = process_data[1]
@@ -49,25 +53,31 @@ def kill_process():
             cmd = ' '.join(process_data[10:])
             if grep in cmd or grep in name or grep == pid:
                 options.append(pid + ' ' + name + ' ' + cmd)
+
     if not options:
         error_image = "/home/gus/Desktop/myfiles/Tecnologia/Programming/Python_programs/Linux_Processes/redcross.png"
         eg.msgbox("No valid data was provided. A process couldn´t be found\n\n\n                Press ok to get back to the main menu", "Error - No data provided", image=error_image)
         return
-    choice = eg.choicebox("Total amount of processes running: "+str(process_amount)+"\n\nChoose a process from the list.\n\nTo Kill the process, double clicked on it, or select it and click Ok.\n\nATTENTION: You will be warned, and have to confirm the killing of the process, in another window instance:", title="Running processes window", choices=options)
-    if choice is None:
-        return
-    pid = choice.split()[0]
-    warning_image="/home/gus/Desktop/myfiles/Tecnologia/Programming/Python_programs/Linux_Processes/warning.png"
-    choices=["Yes", "No"]
-    choice = eg.buttonbox("WARNING!: You have requested to terminate process: "+str(pid)+".\n\n\n                  Do you confirm the termination of this process?", title="Confirm process kill", choices=choices, image=warning_image)
-    if choice == "Yes":
-        kill_process_img="/home/gus/Desktop/myfiles/Tecnologia/Programming/Python_programs/Linux_Processes/process_kill.png"
-        os.popen("sudo kill -9 " + pid)
-        eg.msgbox("Process with PID " + pid + " has been killed.\n\nPress Ok to get back to the processes screen", "Success", image=kill_process_img)
-    if choice == "No":
-        abort_image="/home/gus/Desktop/myfiles/Tecnologia/Programming/Python_programs/Linux_Processes/abort.png"
-        eg.msgbox("You decided to abort the termination of PID: "+str(pid)+"\n\n", title="Aborted Kill process", image=abort_image)
-
+    else:
+        choice = eg.choicebox("Total amount of processes running: "+str(process_amount)+"\n\nChoose a process from the list.\n\nTo Kill the process, double clicked on it, or select it and click Ok.\n\nATTENTION: You will be warned, and have to confirm the killing of the process, in another window instance:", title="Running processes window", choices=options)
+        if choice == "-":
+            error_image = "/home/gus/Desktop/myfiles/Tecnologia/Programming/Python_programs/Linux_Processes/redcross.png"
+            eg.msgbox("ERROR: This option is not selectable.\n\n\n                Press ok to get back to the prompt", "Error - Unselectable option", image=error_image)
+            kill_process()
+        if choice is None:
+            return
+        else:
+            pid = choice.split()[0]
+            warning_image="/home/gus/Desktop/myfiles/Tecnologia/Programming/Python_programs/Linux_Processes/warning.png"
+            choices=["Yes", "No"]
+            choice = eg.buttonbox("WARNING!: You have requested to terminate process: "+str(pid)+".\n\n\n                  Do you confirm the termination of this process?", title="Confirm process kill", choices=choices, image=warning_image)
+            if choice == "Yes":
+                kill_process_img="/home/gus/Desktop/myfiles/Tecnologia/Programming/Python_programs/Linux_Processes/process_kill.png"
+                os.popen("sudo kill -9 " + pid)
+                eg.msgbox("Process with PID " + pid + " has been killed.\n\nPress Ok to get back to the processes screen", "Success", image=kill_process_img)
+            if choice == "No":
+                abort_image="/home/gus/Desktop/myfiles/Tecnologia/Programming/Python_programs/Linux_Processes/abort.png"
+                eg.msgbox("You decided to abort the termination of PID: "+str(pid)+"\n\n", title="Aborted Kill process", image=abort_image)
 
 while True:
     main_image="/home/gus/Desktop/myfiles/Tecnologia/Programming/Python_programs/Linux_Processes/linux_processes.png"
